e09d0797f6f079641c11c4713ba051958a6ecc4c
Maria Mazur <mmazur18@yandex.ru>
2024-05-07 17:35:38 +0300

    Revert "fix"
    This reverts commit e41d08fc6655dbda38c63a940ec789ae9905c01c.

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,33 +1,19 @@
 import arrow from 'src/images/arrow.svg';
-import clsx from 'clsx';
+
 import styles from './ArrowButton.module.scss';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-type ArrowButtonProps = {
-	isOpen: boolean;
-	handler?: OnClick;
-};
-
-export const ArrowButton = ({ isOpen, handler }: ArrowButtonProps) => {
+export const ArrowButton = () => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
-			onClick={handler}
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={clsx(styles.container, {
-				[styles.container_open]: isOpen,
-			})}>
-			<img
-				src={arrow}
-				alt='иконка стрелочки'
-				className={clsx(styles.arrow, {
-					[styles.arrow_open]: isOpen,
-				})}
-			/>
+			className={styles.container}>
+			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,131 +1,14 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
-import { Text } from 'components/text';
-import { Select } from 'components/select';
-
-import { FormEvent, useEffect, useRef, useState } from 'react';
-import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
-import {
-	OptionType,
-	backgroundColors,
-	contentWidthArr,
-	defaultArticleState,
-	fontColors,
-	fontFamilyOptions,
-	fontSizeOptions,
-} from 'src/constants/articleProps';
-import { RadioGroup } from '../radio-group';
-import { Separator } from '../separator';
 
-type ArticleParamsFormProps = {
-	formState: typeof defaultArticleState;
-	setFormState: React.Dispatch<
-		React.SetStateAction<typeof defaultArticleState>
-	>;
-	resetHadnler: () => void;
-	applyHadnler: () => void;
-};
-
-export const ArticleParamsForm = ({
-	formState,
-	setFormState,
-	resetHadnler,
-	applyHadnler,
-}: ArticleParamsFormProps) => {
-	const [isOpen, setOpen] = useState(false);
-	const toggleOpen = (opened: boolean) => setOpen(!opened);
-
-	const formRef = useRef<HTMLFormElement | null>(null);
-	const changeFontFamily = (value: OptionType) =>
-		setFormState({ ...formState, fontFamilyOption: value });
-	const changeFontSize = (value: OptionType) =>
-		setFormState({ ...formState, fontSizeOption: value });
-	const changeFontColor = (value: OptionType) =>
-		setFormState({ ...formState, fontColor: value });
-	const changeBackgroundColor = (value: OptionType) =>
-		setFormState({ ...formState, backgroundColor: value });
-	const changeContentWidth = (value: OptionType) =>
-		setFormState({ ...formState, contentWidth: value });
-
-	const handleEsc = (event: KeyboardEvent) => {
-		if (event.key === 'Escape') toggleOpen(true);
-	};
-
-	const handleOutsideClick = (event: MouseEvent) => {
-		if (
-			event.target instanceof Node &&
-			!formRef.current?.contains(event.target)
-		)
-			toggleOpen(true);
-	};
-
-	useEffect(() => {
-		window.addEventListener('keydown', handleEsc);
-		window.addEventListener('mousedown', handleOutsideClick);
-		return () => {
-			window.removeEventListener('keydown', handleEsc);
-			window.removeEventListener('mousedown', handleOutsideClick);
-		};
-	}, [isOpen]);
-
-	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		applyHadnler();
-	};
-
+export const ArticleParamsForm = () => {
 	return (
 		<>
-			<ArrowButton handler={() => toggleOpen(isOpen)} isOpen={isOpen} />
-			<aside
-				className={clsx(styles.container, {
-					[styles.container_open]: isOpen,
-				})}
-				ref={formRef}>
-				<form
-					className={styles.form}
-					onSubmit={handleSubmit}
-					onReset={resetHadnler}>
-					<Text as={'h2'} size={31} weight={800} uppercase={true}>
-						Задайте параметры
-					</Text>
-					<Select
-						selected={formState.fontFamilyOption}
-						options={fontFamilyOptions}
-						placeholder='Выберите шрифт'
-						title='шрифт'
-						onChange={changeFontFamily}
-					/>
-					<RadioGroup
-						name='fontSize'
-						options={fontSizeOptions}
-						selected={formState.fontSizeOption}
-						title='размер шрифта'
-						onChange={changeFontSize}
-					/>
-					<Select
-						selected={formState.fontColor}
-						options={fontColors}
-						placeholder='Выберите цвет'
-						title='цвет шрифта'
-						onChange={changeFontColor}
-					/>
-					<Separator />
-					<Select
-						selected={formState.backgroundColor}
-						options={backgroundColors}
-						placeholder='Выберите цвет'
-						title='цвет фона'
-						onChange={changeBackgroundColor}
-					/>
-					<Select
-						selected={formState.contentWidth}
-						options={contentWidthArr}
-						placeholder='Выберите ширину'
-						title='ширина контента'
-						onChange={changeContentWidth}
-					/>
+			<ArrowButton />
+			<aside className={styles.container}>
+				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
